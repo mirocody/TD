83232a703507be12ea202e1b39adc07fa1628779
NekoPunch <wenxin5291311@sina.com>
2016-06-23 19:30:49 -0700

    Version 6.0

---
+++ Assets/Script/GameController/ViewDrag.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewDrag : MonoBehaviour
+{
+    public float speed;
+    bool isDown = false;
+    Vector3 offset;
+    Vector3 lastPos;
+    Vector3 nowPos;
+    Vector3 worldLowerLeft;
+    Vector3 worldUpperRight;
+
+    void Start()
+    {
+        //speed = 1.0f;
+        GetComponent<Camera>().orthographicSize = Mathf.Max(GetComponent<Camera>().orthographicSize, 30f);
+        worldLowerLeft = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        worldUpperRight = Camera.main.ViewportToWorldPoint(Vector3.one);
+        isDown = false;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            if (Input.GetMouseButtonDown(0) && !isDown)
+            {
+                isDown = true;
+                lastPos = Input.mousePosition;
+            }
+            else if (Input.GetMouseButtonUp(0)) isDown = false;
+            if (isDown)
+            {
+                Vector3 minCorner = Camera.main.ViewportToWorldPoint(Vector3.zero);
+                Vector3 maxCorner = Camera.main.ViewportToWorldPoint(Vector3.one);
+                Vector3 boundingBoxSize = (maxCorner - minCorner);
+                float limXLow = worldLowerLeft.x + boundingBoxSize.x / 2;
+                float limXHigh = worldUpperRight.x - boundingBoxSize.x / 2;
+                float limYLow = worldLowerLeft.z + boundingBoxSize.z / 2;
+                float limYHigh = worldUpperRight.z - boundingBoxSize.z / 2;
+                nowPos = Input.mousePosition;
+                offset = nowPos - lastPos;
+                offset *= speed * Time.deltaTime;
+                Vector3 camPos = transform.position;
+                float x = Mathf.Clamp(camPos.x - offset.x, limXLow, limXHigh);
+                float z = Mathf.Clamp(camPos.z - offset.y, limYLow, limYHigh);
+                transform.position = new Vector3(x, camPos.y, z);
+                lastPos = nowPos;
+            }
+        }
+    }
+}
---
+++ Assets/Script/NewMap.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class NewMap : MonoBehaviour {
+
+	public GameObject route;
+	public GameObject towerspot;
+	public GameObject path;
+	public TextAsset csvLevel;
+
+
+	private static int columns;
+	private static int rows;
+	private static int totalPathPoint;
+
+	public int[,] map;
+	public static GameObject[] pathPoints;
+
+
+	// Use this for initialization
+	void Start () {
+		InitialMap ();
+
+		for (int row = 0; row < rows; row++) {
+			for (int column = 0; column < columns; column++) {
+				if (map [row, column] == 2) {
+					Vector3 position = new Vector3 ((float)(column - 15), 0.07f, -(float)(row) + 2);
+					GameObject temp = (GameObject)Instantiate (towerspot, position, this.transform.rotation);
+                    temp.name+=("_"+row+"_"+column);
+				} else if (map [row, column] == 3) {
+					Vector3 position = new Vector3 ((float)(column - 15), 0.07f, -(float)(row) + 2);
+					Instantiate (route, position, this.transform.rotation);
+
+				} else if (map [row, column] >= 5) {
+					Vector3 position = new Vector3 ((float)(column - 15), 0.07f, -(float)(row) + 2);
+					Instantiate (route, position, this.transform.rotation);
+					
+					GameObject pathpoint = (GameObject)Instantiate (path, position, this.transform.rotation);
+					pathPoints [map [row, column] - 5] = pathpoint;
+				}
+
+			}
+		}
+	}
+
+	void InitialMap () {
+		string[] lines = csvLevel.text.Split ('\n');
+		string line1 = lines [0];
+		// get the map count data;
+		string[] map_count = line1.Split (',');
+
+		totalPathPoint = int.Parse (map_count [1]);
+		columns = int.Parse (map_count [3]);
+		rows = int.Parse (map_count [5]);
+		pathPoints = new GameObject[totalPathPoint];
+		map = new int[rows, columns];
+
+		for(int i = 1; i < lines.GetLength(0); i++) {
+			string[] map_choice = lines[i].Split (',');
+			int j = 0;
+			foreach (string choice in map_choice) {
+				map [i-1, j] = int.Parse (choice);
+				Debug.Log (i + "," + j + "\n");
+				j++;
+			}
+		}
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
